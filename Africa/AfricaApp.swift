3dce91b996fab4781862c0250c5505e39ccc2f32
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-11-24 14:36:51 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Vatana Chhorn on 24/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
