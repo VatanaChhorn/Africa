0670b3732248759c884b0b1499251a4143bc3d01
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-12-29 13:42:14 +0700

    Finished section 31

--- Africa/MapView.swift
+++ Africa/MapView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region : MKCoordinateRegion = {
+        let mapCoordinate = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        let mapZoomLabel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        let mapRegion = MKCoordinateRegion(center: mapCoordinate, span: mapZoomLabel)
+        return mapRegion
+    } ()
+    
+    let locations : [NationalParkLocation] = Bundle.main.decodable("locations.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Map")
+//        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+       
+//            MapMarker (coordinate: item.location, tint: .accentColor)
+            
+            
+            /* Custom Basic Annotatipm*/
+            MapAnnotation (coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }  //: ANNOTATION
+        }
     }
 }
 
+// MARK: - PREVIEW
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
